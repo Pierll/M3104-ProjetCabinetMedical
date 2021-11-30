0b6d236868920965d690fff082a83fd306545bf8
Strowiz <ulysse82100@gmail.com>
2021-11-30 12:04:56 +0100

    First commit zebi

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<h1>Bienvenue sur notre site</h1><br/><br/>
+		<h2><a href = "./usager.php"> Gestion Usagers </h2><br/><br/>
+		<h2><a href = "./medecin.php"> Gestion Médecins </h2><br/><br/>
+		
+	</body>
+</html>
