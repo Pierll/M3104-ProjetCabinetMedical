7e7394eb37515a7e90c6a3fefc421dfc067e3c83
Pierll <51515871+Pierll@users.noreply.github.com>
2021-12-28 18:04:32 +0100

    ajout medecin + usager

--- ajoutmedecin.php
+++ ajoutmedecin.php
@@ -1,32 +1,53 @@
+<!DOCTYPE HTML>
+<html>
+	<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<?php include 'requires/require_login.php'; ?>
+		<?php include 'requires/require_db.php'; ?>		
+		<p>Saisir l'usager : </p>
+		<form method="post">
+		   Civilite: <select name="civ">
+			   <option value="M">Monsieur</option>
+		       <option value="Mme">Madame</option>
+			</select>
+			<p> Nom <input type="text" name="nom" /></p>
+			<p> Prénom <input type="text" name="prenom" /></p>
+			<p><input name="btn" type="submit" value="Envoyer">
+			<input type="reset" value="Vider"></p>
+		</form>
+
 <?php 
-$server = "localhost";
-	$login = "root";
-	$mdp = "password";
-	$db = "CabinetMedical";
+			if (isset($_POST["btn"])) {
+				foreach ($_POST as $param_name => $param_val) {
+				    if (!isset($_POST[$param_name]) || $_POST[$param_name] == '') {
+				    	echo "Erreur lors du traitement, une valeur est manquante: Param: $param_name; Value: $param_val\n";
+				    	exit();
+				    }
+					
+				}	
+				if ($_POST["civ"] == "Mme") {
+					//echo '<p>MADAME</p>';
+					$civilite = 0;
+				} else {
+					//echo '<p>MONSIEUR</p>';
+					$civilite = 1;
+				}
+			    $ajoutusager = $linkpdo->prepare('INSERT INTO Medecin(Civilite, Nom, Prenom) values (?,?,?)');
+				
+				try {
+					$ajoutusager->execute(array($civilite, $_POST["nom"],$_POST["prenom"]));
+				} catch (PDOException $e) {
+					print $e;
+					die('Erreur');
+				}
+				
 
-	//Connexion au serveur MySQL 
-	try { 
-		  $linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp); 
-	} catch (Exception $e) { 
-		die('Erreur : ' . $e->getMessage());
-	}
-	$linkpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	foreach ($_POST as $param_name => $param_val) {
-		if (!isset($_POST[$param_name]) || $_POST[$param_name] == '') {
-			echo "Erreur lors du traitement, une valeur est manquante: Param: $param_name; Value: $param_val\n";
-			exit();
-		}
-
-	}
-	$ajoutcontact = $linkpdo->prepare('INSERT INTO Medecin (civilite, nom, prenom) values (?,?,?)');
-
-	try {
-		$ajoutcontact->execute(array($_POST["civilite"],$_POST["nom"],$_POST["prenom"]));
-	} catch (PDOException $e) { 
-		print($e);
-		//die('Erreur, le numero est deja present dans la base de donnee');
-		die('Une erreur est survenue');
-	}
-	echo 'Le numero a ete correctement enregistre';
+			}
 ?>
+	</body>
+</html>
+
+
--- ajoutusager.php
+++ ajoutusager.php
@@ -1,32 +1,72 @@
+<!DOCTYPE HTML>
+<html>
+	<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<?php include 'requires/require_login.php'; ?>
+		<?php include 'requires/require_db.php'; ?>
+		<p>Saisir l'usager : </p>
+		<form method="post">
+		   Civilite: <select name="civ">
+			   <option value="1">Monsieur</option>
+		       <option value="0">Madame</option>
+			</select>
+			<p> Nom <input type="text" name="nom" /></p>
+			<p> Prénom <input type="text" name="prenom" /></p>
+			<p> Adresse <input type="text" name="adresse" /></p>
+			<p> Date de naissance <input type="text" name="datenaissance" /></p>
+			<p> Lieu de naissance <input type="text" name="lieunaissance" /></p>
+			<p> Numéro de sécu <input type="text" name="numsecu" /></p>
+			<?php // liste deroulante des medecins 
+			$medecins = $linkpdo->prepare("SELECT Id_Medecin, CONCAT(Nom, ' ', Prenom) AS NomPrenom FROM Medecin");
+			$medecins->execute();
+			$result = $medecins->fetchAll(PDO::FETCH_ASSOC); //pour ne avoir de doublons
+			if (empty($result)) {
+				echo '<b>Pas de resultats trouves<b>';
+			} else {
+				echo "Medecin Referant :";
+				echo "<select name=\"idmedecin\">";
+				foreach ($result as $r) {
+					//print_r($r);
+					echo '<option value='.$r['Id_Medecin'].'>'.$r['NomPrenom'];
+					
+					echo '<br/>';
+				}
+				echo "</select>";
+			}
+			?>
+			<p><input name="btn" type="submit" value="Envoyer">
+			<input type="reset" value="Vider"></p>
+
+		</form>
+
+			
+
 <?php 
-$server = "localhost";
-	$login = "root";
-	$mdp = "password";
-	$db = "CabinetMedical";
+			if (isset($_POST["btn"])) {
+				foreach ($_POST as $param_name => $param_val) {
+				    if (!isset($_POST[$param_name]) || $_POST[$param_name] == '') {
+				    	echo "Erreur lors du traitement, une valeur est manquante: Param: $param_name; Value: $param_val\n";
+				    	exit();
+				    }
+					
+				}	
 
-	//Connexion au serveur MySQL 
-	try { 
-		  $linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp); 
-	} catch (Exception $e) { 
-		die('Erreur : ' . $e->getMessage());
-	}
-	$linkpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			    $ajoutusager = $linkpdo->prepare('INSERT INTO Usager(Civilite, Nom, Prenom, Adresse, DateNaissance, LieuNaissance, NumSecu, Id_Medecin) values (?,?,?,?,?,?,?,?)');
+				
+				try {
+					$ajoutusager->execute(array($_POST["civ"], $_POST["nom"],$_POST["prenom"], $_POST["adresse"], $_POST["datenaissance"], $_POST["lieunaissance"], $_POST["numsecu"], $_POST["idmedecin"]));
+				} catch (PDOException $e) {
+					print $e;
+					die('Erreur');
+				}
+				
 
-	foreach ($_POST as $param_name => $param_val) {
-		if (!isset($_POST[$param_name]) || $_POST[$param_name] == '') {
-			echo "Erreur lors du traitement, une valeur est manquante: Param: $param_name; Value: $param_val\n";
-			exit();
-		}
-
-	}
-	$ajoutcontact = $linkpdo->prepare('INSERT INTO Usager (civilite, nom, prenom, adresse, dateNaissance, lieuNaissance, numSecu) values (?,?,?,?,?,?,?)');
-
-	try {
-		$ajoutcontact->execute(array($_POST["civilite"],$_POST["nom"],$_POST["prenom"],$_POST["adresse"],$_POST["dateNaissance"],$_POST["lieuNaissance"],$_POST["numSecu"]));
-	} catch (PDOException $e) { 
-		print($e);
-		//die('Erreur, le numero est deja present dans la base de donnee');
-		die('Une erreur est survenue');
-	}
-	echo 'Le numero a ete correctement enregistre';
+			}
 ?>
+	</body>
+</html>
+
+
